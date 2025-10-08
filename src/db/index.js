821b5baea512d72e576bdee1dac003be79a9f1b0
arnob <farhanasfar21@gmail.com>
2025-10-08 18:51:01 +0600

    Make 'avatar' optional for user signup

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async() => {
     try {
-        console.log(process.env.MONGODB_URI);
+        //console.log(process.env.MONGODB_URI);
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
         
         console.log(`MongoDB Connected...HOST:${connectionInstance.connection.host}`);
