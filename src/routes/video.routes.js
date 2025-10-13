be888207d6ace47a5967fc5f5b503b40f5ebfcdc
arnob <farhanasfar21@gmail.com>
2025-10-14 01:03:53 +0600

    Fix route

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -20,9 +20,9 @@ videoRouter.route("/publish-video").post(
 
 videoRouter.route("/all-videos").get(getAllVideos);
 
-videoRouter.route("/get-users-video/:id").get(getVideosByUser);
+videoRouter.route("/get-users-video/:userId").get(getVideosByUser);
 
-videoRouter.route("/delete-video/:id").delete(verifyJWT, deleteVideo);
+videoRouter.route("/delete-video/:userId").delete(verifyJWT, deleteVideo);
 
 
 export {videoRouter};
