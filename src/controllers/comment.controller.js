df7f575a28a0ba1286f8e488cbeb512a90c9ecba
arnob <farhanasfar21@gmail.com>
2025-10-21 00:39:58 +0600

    Complete 'addComment' controtroller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,10 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
-
 import { Comment } from "../models/comment.model.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js"
-
 
 
 const addComment = asyncHandler(async (req, res) => {
